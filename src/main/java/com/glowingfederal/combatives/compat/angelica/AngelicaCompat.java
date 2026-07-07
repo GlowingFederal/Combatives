b43bc84690fa261e0d36a44f84ef3d90b139d860
Zealot <39095332+GlowingFederal@users.noreply.github.com>
2026-07-07 15:17:08 -0400

    Establish clean Combatives foundation

---
+++ src/main/java/com/glowingfederal/combatives/compat/angelica/AngelicaCompat.java
@@ -0,0 +1,9 @@
+package com.glowingfederal.combatives.compat.angelica;
+
+/**
+ * Placeholder for future Combatives AngelicaCompat implementation.
+ */
+public final class AngelicaCompat {
+    private AngelicaCompat() {
+    }
+}
---
+++ src/main/java/com/glowingfederal/combatives/proxy/CommonProxy.java
@@ -0,0 +1,12 @@
+package com.glowingfederal.combatives.proxy;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    public void init(FMLInitializationEvent event) {
+    }
+}

827ab93ec2b069b136718515591d9b87e2523548
Zealot <39095332+GlowingFederal@users.noreply.github.com>
2026-07-08 23:13:51 -0400

    dada

--- src/main/java/com/glowingfederal/combatives/mixin/EntityRendererMixin.java
+++ src/main/java/com/glowingfederal/combatives/mixin/EntityRendererMixin.java
@@ -2,7 +2,6 @@ package com.glowingfederal.combatives.mixin;
 
 import com.glowingfederal.combatives.client.camera.CameraCompatibilityManager;
 import com.glowingfederal.combatives.client.camera.CameraController;
-import com.glowingfederal.combatives.entity.player.ICombativesPlayerPose;
 import com.glowingfederal.combatives.util.math.MathHelperNew;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
@@ -26,8 +25,6 @@ public abstract class EntityRendererMixin {
     private float combatives$previousEyeHeight;
     private float combatives$entityEyeHeight;
     private float combatives$partialTicks;
-    private boolean combatives$renderingHand;
-
 
     @Inject(method = "updateCameraAndRender", at = @At("HEAD"))
     private void combatives$updateCamera(float partialTicks, CallbackInfo ci) {
@@ -38,44 +35,9 @@ public abstract class EntityRendererMixin {
         }
     }
 
-    @Inject(method = "orientCamera", at = @At("TAIL"))
-    private void combatives$applyCameraTransforms(float partialTicks, CallbackInfo ci) {
-        CameraController.INSTANCE.applyTransforms(partialTicks);
-    }
-
-
-    @Inject(method = "renderHand", at = @At("HEAD"))
-    private void combatives$beginHandRender(float partialTicks, int pass, CallbackInfo ci) {
-        this.combatives$renderingHand = true;
-    }
-
-    @Inject(method = "renderHand", at = @At("RETURN"))
-    private void combatives$endHandRender(float partialTicks, int pass, CallbackInfo ci) {
-        this.combatives$renderingHand = false;
-    }
-
-    @Inject(method = "setupViewBobbing", at = @At("HEAD"), cancellable = true)
-    private void combatives$cancelVanillaViewBobbing(float partialTicks, CallbackInfo ci) {
-        if (!this.combatives$renderingHand && CameraCompatibilityManager.shouldCancelVanillaViewBobbing()) {
-            ci.cancel();
-        }
-    }
-
-    @Inject(method = "getFOVModifier", at = @At("RETURN"), cancellable = true)
-    private void combatives$applyMovementFov(float partialTicks, boolean useFOVSetting, CallbackInfoReturnable<Float> cir) {
-        if (CameraCompatibilityManager.ownsDynamicFov()) {
-            cir.setReturnValue(cir.getReturnValue() * (1.0F + CameraController.INSTANCE.getFovModifier()));
-        }
-    }
-
-
-    @Inject(method = "updateCameraAndRender", at = @At("HEAD"))
-    private void combatives$updateCamera(float partialTicks, CallbackInfo ci) {
-        if (this.mc.thePlayer instanceof EntityPlayerSP) {
-            CameraController.INSTANCE.update(this.mc, (EntityPlayerSP) this.mc.thePlayer, partialTicks);
-        } else {
-            CameraController.INSTANCE.reset();
-        }
+    @Inject(method = "orientCamera", at = @At("HEAD"))
+    private void combatives$capturePartialTicks(float partialTicks, CallbackInfo ci) {
+        this.combatives$partialTicks = partialTicks;
     }
 
     @Inject(method = "orientCamera", at = @At("TAIL"))
@@ -83,37 +45,13 @@ public abstract class EntityRendererMixin {
         CameraController.INSTANCE.applyTransforms(partialTicks);
     }
 
-    @Inject(method = "setupViewBobbing", at = @At("HEAD"), cancellable = true)
-    private void combatives$cancelVanillaViewBobbing(float partialTicks, CallbackInfo ci) {
-        if (CameraCompatibilityManager.ownsViewBobbing()) {
-            ci.cancel();
-        }
-    }
-
-    @Inject(method = "getFOVModifier", at = @At("RETURN"), cancellable = true)
-    private void combatives$applyMovementFov(float partialTicks, boolean useFOVSetting, CallbackInfoReturnable<Float> cir) {
-        if (CameraCompatibilityManager.ownsDynamicFov()) {
-            cir.setReturnValue(cir.getReturnValue() * (1.0F + CameraController.INSTANCE.getFovModifier()));
-        }
-    }
-
-
-    @Inject(method = "updateCameraAndRender", at = @At("HEAD"))
-    private void combatives$updateCamera(float partialTicks, CallbackInfo ci) {
-        if (this.mc.thePlayer instanceof EntityPlayerSP) {
-            CameraController.INSTANCE.update(this.mc, (EntityPlayerSP) this.mc.thePlayer, partialTicks);
-        } else {
-            CameraController.INSTANCE.reset();
-        }
-    }
-
-    @Inject(method = "orientCamera", at = @At("TAIL"))
-    private void combatives$applyCameraTransforms(float partialTicks, CallbackInfo ci) {
-        CameraController.INSTANCE.applyTransforms(partialTicks);
-    }
-
-
-    @Inject(method = "renderHand", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemRenderer;renderItemInFirstPerson(F)V"))
+    @Inject(
+            method = "renderHand",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/ItemRenderer;renderItemInFirstPerson(F)V"
+            )
+    )
     private void combatives$applyHandBobbing(float partialTicks, int pass, CallbackInfo ci) {
         CameraController.INSTANCE.applyHandTransforms(partialTicks);
     }
@@ -132,19 +70,29 @@ public abstract class EntityRendererMixin {
         }
     }
 
-    @Inject(method = "orientCamera", at = @At("HEAD"))
-    private void combatives$capturePartialTicks(float partialTicks, CallbackInfo ci) {
-        this.combatives$partialTicks = partialTicks;
-    }
-
-    @ModifyVariable(method = "orientCamera", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/EntityLivingBase;prevPosX:D", ordinal = 0), ordinal = 1)
+    @ModifyVariable(
+            method = "orientCamera",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/entity/EntityLivingBase;prevPosX:D",
+                    ordinal = 0
+            ),
+            ordinal = 1
+    )
     private float combatives$getInterpolatedEyeHeight(float eyeHeight) {
         Entity entity = this.mc.renderViewEntity;
+
         if (!(entity instanceof EntityPlayer)) {
             return eyeHeight;
         }
+
         this.combatives$entityEyeHeight = ((EntityPlayer) entity).getEyeHeight();
-        return MathHelperNew.lerp(this.combatives$partialTicks, this.combatives$previousEyeHeight, this.combatives$eyeHeight);
+
+        return MathHelperNew.lerp(
+                this.combatives$partialTicks,
+                this.combatives$previousEyeHeight,
+                this.combatives$eyeHeight
+        );
     }
 
     @Inject(method = "updateRenderer", at = @At("TAIL"))
